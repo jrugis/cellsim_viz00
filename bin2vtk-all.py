461b5a7817137c96ed1f2669d3f3a82f3fe08653
jrugis <j.rugis@auckland.ac.nc>
2016-05-16 20:00:48 +1200

    initial edits

--- bin2vtk-all.py
+++ bin2vtk-all.py
@@ -8,6 +8,7 @@
 #
 
 import numpy as np
+import os
 import subprocess
 from evtk.hl import pointsToVTK
 import cs
@@ -17,28 +18,29 @@ import cs
 ##################################################################
 
 dist_names = subprocess.check_output("ls *.bin", shell=True).split()
-print dist_names
 
-#d = {}
-#for i in range(len(dist_names)):
-#  fname = dist_names[i]
-#  dist = cs.get_data(fname)[:,0]
-#  d[fname.split('.')[0]] = dist
+for i in range(len(dist_names)):
+  dist_name = dist_names[i]
+  cell_name = dist_name.split('_')[0]
+  mesh_name =  cell_name + 'm_HARMONIC_100p.msh'
+  print '*** ' + cell_name + ' ***'
+  print 'reading mesh file: ' + mesh_name 
+  x,y,z = cs.get_mesh_coords(mesh_name)
+  print 'reading data file: ' + dist_name 
+  dist = cs.get_data(dist_name)
 
-# set the simulation time scale
-#i_start = 828
-#i_end = 1811
+  # get start and finish indices for a single cycle
+  i_start = 100
+  i_finish = 120
 
-# get the mesh coordinates
-#x,y,z = cs.get_mesh_coords(meshfname + ".msh")
-
-# get calcium data
-#c = cs.get_data(cfname + ".bin")
-
-# write vtk time series files
-#for i in xrange(i_start, i_end, 1):
-#  fname = fdir + "cell" + str(i)
-#  d = {}
-#  d["c"] = c[:, i]
-#  pointsToVTK(fname, x, y, z, data = d)
+  # write vtk time series files
+  if os.path.isdir(cell_name):
+    os.system("rm -rf " + cell_name)
+  os.mkdir(cell_name)
+  for j in xrange(i_start, i_finish, 1):
+    fname = cell_name + '/' + cell_name + '_' + str(j).zfill(4)   
+    print fname   
+    d = {}
+    d["c"] = dist[:, j]
+    pointsToVTK(fname, x, y, z, data = d)
 
