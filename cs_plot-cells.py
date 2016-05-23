16ed23a337e0f1d8f69e5aaa6009a6a0897ea4b5
jrugis <j.rugis@auckland.ac.nc>
2016-05-23 19:56:30 +1200

    add glyph spheres and cell stls

--- cs_plot-cells.py
+++ cs_plot-cells.py
@@ -12,7 +12,11 @@ import cs
 # plot the calcium data
 dist_names = subprocess.check_output("ls *.bin", shell=True).split()
 plt.rcParams['axes.color_cycle'] = ['r', 'g', 'b']
+plt.rcParams['axes.titlesize'] = 'small'
+plt.rcParams['figure.figsize'] = 5, 15
 fig, plots = plt.subplots(len(dist_names), 1, sharex='col')
+fig.subplots_adjust(hspace = 0.4)
+plots[len(dist_names) - 1].set_ylabel('Ca')
 
 for i in range(len(dist_names)):
   cell_name = dist_names[i].split('_')[0]
@@ -20,11 +24,10 @@ for i in range(len(dist_names)):
   data = cs.get_data(dist_names[i])
   max_per_row = np.amax(data, axis=1)
   rows = [np.argmax(max_per_row), np.argmin(max_per_row)]
-  plots[i].set_ylabel(cell_name)
+  plots[i].set_title(dist_names[i].split('.')[0])
   plots[i].set_ylim([0.0, 0.8])
   plots[i].plot(np.transpose(data[rows, :]), lw=0.5)
 
-fig.set_size_inches(5, 13)
 open('temp.pdf', 'w').close()
 plt.savefig('temp.pdf')
 os.rename('temp.pdf', 'seven_cells.pdf')
