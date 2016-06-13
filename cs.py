33a0998d182e75e61e95aa7243dd6f99d1fd3e19
jrugis <j.rugis@auckland.ac.nc>
2016-06-13 19:15:57 +1200

    added more stats

--- cs.py
+++ cs.py
@@ -12,22 +12,54 @@ def get_mesh_coords(fname):
   f1 = open(fname, 'r')
   for line in f1: 
     if line.startswith("$Nodes"): break
-  for line in f1:
-    pcount = int(line)
-    break
-  x = np.empty((pcount))
-  y = np.empty((pcount))
-  z = np.empty((pcount))
+  ncount = int(f1.next())
+  xyz = np.empty((ncount, 3))
   t = 0
   for line in f1:
     v = line.split()
-    x[t] = float(v[1])
-    y[t] = float(v[2])
-    z[t] = float(v[3])
+    xyz[t] = [float(v[1]), float(v[2]), float(v[3])]
     t += 1
-    if t == pcount: break
+    if t == ncount: break
   f1.close
-  return x, y, z
+  return xyz
+##################################################################
+# get the mesh elements (i.e. surface triangles and tetrahedrons)
+def get_mesh_elements(fname):
+  f1 = open(fname, 'r')
+  for line in f1: 
+    if line.startswith("$Elements"): break
+  ecount = int(f1.next())
+  tt = np.zeros((ecount, 4), dtype = np.uint32)
+  t = 0
+  tricount = 0
+  tetcount = 0
+  for line in f1:       # assumes all tris listed before tets
+    v = line.split()
+    if int(v[1]) == 2:   
+      tt[t] = [int(v[5])-1, int(v[6])-1, int(v[7])-1, 0] # change to zero indexing
+      tricount += 1
+    elif int(v[1]) == 4:
+      tt[t] = [int(v[5])-1, int(v[6])-1, int(v[7])-1, int(v[8])-1] # change to zero indexing
+      tetcount += 1
+    t += 1
+    if t == ecount: break
+  f1.close
+  return tt[0:tricount, 0:3], tt[tricount:, :] 
+##################################################################
+# get the distance to nearest lumen (dnl) data
+def get_mesh_dnl(fname):
+  f1 = open(fname, 'r')
+  for line in f1: 
+    if line.startswith('"distance to nearest lumen"'): break
+  for t in range(5): # skip 5 lines
+    f1.next()
+  ncount = int(f1.next())
+  dnl = np.empty(ncount)
+  for t in range(ncount):
+    v = f1.next().split()
+    dnl[t] = float(v[1])
+  f1.close
+  return dnl
 ##################################################################
 # read in a bin data file
 def get_data(fname):
--- membrane_dist.py
+++ membrane_dist.py
@@ -2,43 +2,38 @@ import numpy as np
 import subprocess
 import os
 
+import cs
+
 ##################################################################
 # functions
 ##################################################################
 
 def get_membrane_data(fname):
-  f1 = open(fname + '.msh', 'r') # open the mesh file
+  xyz = cs.get_mesh_coords(fname + '.msh')          # get the node data
+  tris, tets = cs.get_mesh_elements(fname + '.msh') # get the element data
+  dnl = cs.get_mesh_dnl(fname + '.msh')             # get "distance to nearest lumen" data
+  ncount = xyz.shape[0]
 
-  # get the node count
-  for line in f1: 
-    if line.startswith("$Nodes"): break
-  ncount = int(f1.next())
-  print " ", ncount, "total nodes"
+  # calculate surface area
+  area = 0.0
+  for tri in tris:
+    v = np.vstack((xyz[tri[0]], xyz[tri[1]], xyz[tri[2]]))
+    area += np.abs(np.linalg.det(np.array((v[0]-v[2],v[1]-v[2],(1.0,1.0,1.0)))))/2.0
+ 
+  # calculate volume
+  volume = 0.0
+  for tet in tets:
+    v = np.vstack((xyz[tet[0]], xyz[tet[1]], xyz[tet[2]], xyz[tet[3]]))
+    volume += np.abs(np.linalg.det((np.array((v[0]-v[3],v[1]-v[3],v[2]-v[3])))))/6.0
 
   # identify the surface nodes
   sn = np.zeros(ncount, dtype = np.uint8)
-  for line in f1: 
-    if line.startswith("$Elements"): break
-  f1.next()
-  for line in f1:  
-    v = line.split()
-    if int(v[1]) != 2: break # finished with surface elements?
-    for i in range(5, 8):    # mark the surface element nodes
-      sn[int(v[i])] = 1
+  for v in tris: # mark the surface element nodes
+    for i in range(3):
+      sn[v[i]] = 1
   sncount = 0 # surface node count
   for n in sn:
     if n == 1: sncount += 1
-  print " ", sncount, "surface nodes"
-
-  # get "distance to nearest lumen" data
-  for line in f1: 
-    if line.startswith('"distance to nearest lumen"'): break
-  for t in range(6): # skip 6 lines
-    f1.next()
-  dnl = np.empty(ncount)
-  for t in range(ncount):
-    v = f1.next().split()
-    dnl[t] = float(v[1])
 
   # create surface node dnl array
   sndnl = np.zeros(sncount)
@@ -48,21 +43,15 @@ def get_membrane_data(fname):
       sndnl[i] = dnl[n]
       i += 1
 
-  f1.close # close the mesh file 
-  print "  %.2f surface node dnl minimum" % np.amin(sndnl)
-  print "  %.2f surface node dnl maximum" % np.amax(sndnl)
-  print "  %.2f surface node dnl average" % np.mean(sndnl)
-  print "  %.1f surface node percentage of total nodes" % (100.0 * sncount / ncount)
-  app = 100.0 * np.histogram(sndnl, bins=8, range=(0.0, 8.0))[0][0] / sncount
-  print "  %.1f apical percentage of surface nodes (1um dnl cutoff)" % app
-
-  return sndnl
+  extents = np.vstack((np.amin(xyz, axis=0), np.amax(xyz, axis=0)))
+  return ncount, area, volume, sndnl, extents
 
 ##################################################################
 # main program
 ##################################################################
 
 import matplotlib.pyplot as plt
+np.set_printoptions(precision=2)
 
 mesh_names = subprocess.check_output("ls *.msh", shell=True).split()
 plt.rcParams['axes.titlesize'] = 'small'
@@ -71,14 +60,43 @@ fig, plots = plt.subplots(len(mesh_names), sharex='col', sharey='col')
 fig.subplots_adjust(hspace = 0.4)
 fig.suptitle('Surface membrane dnl histograms')
 
+fmin = np.finfo(np.float64).max
+vmin = (fmin,fmin,fmin)
+fmax = np.finfo(np.float64).min
+vmax = (fmax,fmax,fmax)
+bbox = np.array((vmin,vmax))
+
 for i in range(len(mesh_names)):
   fname = mesh_names[i].split('.')[0]
   cell_name = fname.split('_')[0]
   print cell_name
   plots[i].set_title(cell_name)
-  sndnl = get_membrane_data(fname)
+  ncount, area, volume, sndnl, extents = get_membrane_data(fname)
+  sncount = len(sndnl)
+  bbox[0] = np.minimum(bbox[0], extents[0])
+  bbox[1] = np.maximum(bbox[1], extents[1])
+
+  print " ", ncount, "nodes"
+  print "  %.1f volume (cubic um3)" % volume
+  print "  %.1f nodes per volume (nodes / cubic um3)" % (ncount / volume)
+  print " ", sncount, "surface nodes"
+  print "  %.1f surface area (square um)" % area
+  print "  %.1f nodes per surface area (nodes / square um)" % (sncount / area)
+  print "  %.2f surface node dnl minimum (um)" % np.amin(sndnl)
+  print "  %.2f surface node dnl maximum (um)" % np.amax(sndnl)
+  print "  %.2f surface node dnl average (um)" % np.mean(sndnl)
+  print "  %.1f surface node percentage of total nodes" % (100.0 * sncount / ncount)
+  app = 100.0 * np.histogram(sndnl, bins=8, range=(0.0, 8.0))[0][0] / sncount
+  print "  %.1f apical percentage of surface nodes (1um dnl cutoff)" % app
+
   #plots[i].hist(sndnl, bins=16, range=(0.0, 8.0), normed=True, rwidth=0.9)
   plots[i].hist(sndnl, bins=16, range=(0.0, 8.0), rwidth=0.9)
+
+print "coordinates bounding box (um):" 
+print bbox
+bboxc = (bbox[1] + bbox[0]) / 2.0
+print "coordinates bounding box center (um):" 
+print bboxc
 
 plots[len(mesh_names) - 1].set_xlabel('dnl')
 open('temp.pdf', 'w').close()
